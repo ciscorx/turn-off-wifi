78577cd900b308ec2de328f19079ca38367d2678
ciscorx <ciscorx@gmail.com>
2019-09-15 22:18:43 -0500

    edited scheduler and fixed bug that caused wifi to always turn off on boot

--- wifi_scheduler.sh
+++ wifi_scheduler.sh
@@ -1,14 +1,15 @@
 #!/bin/sh -e
 
+sleep 60  # wait a minute to allow sync with time server
 currenttime=$(date +%H:%M)
 currentdayoftheweek=$(date +%a)
+## turn off wifi on boot up if current datetime falls on scheduled off target, otherwise turn it on
 
-## turn off wifi on boot up if current datetime falls on scheduled target, otherwise turn it on
-if ([ "$currenttime" \> "21:00" ] || [ "$currenttime" \< "14:30" ]) && ([ "Mon" = "$currentdayoftheweek" ] || [ "Tue" = "$currentdayoftheweek" ] || [ "Wed" = "$currentdayoftheweek" ] || [ "Thu" = "$currentdayoftheweek" ]); then
+if ([ "$currenttime" \> "21:00" ] || [ "$currenttime" \< "14:30" ]) && ([ "Sun" = "$currentdayoftheweek" ] || [ "Mon" = "$currentdayoftheweek" ] || [ "Tue" = "$currentdayoftheweek" ] || [ "Wed" = "$currentdayoftheweek" ] || [ "Thu" = "$currentdayoftheweek" ]); then
     /usr/bin/emacs24-x -Q -l /home/pi/scripts/disable_wifi.el	
 elif [ "$currenttime" \< "14:30" ] && [ "Fri" = "$currentdayoftheweek" ] ; then
         /usr/bin/emacs24-x -Q -l /home/pi/scripts/disable_wifi.el   
-elif [ "$currenttime" \< "08:00" ] && ([ "Sat" = "$currentdayoftheweek" ] || [ "Sun" = "$currentdayoftheweek" ]); then
+elif [ "$currenttime" \< "06:45" ] && ([ "Sat" = "$currentdayoftheweek" ] || [ "Sun" = "$currentdayoftheweek" ]); then
     /usr/bin/emacs24-x -Q -l /home/pi/scripts/disable_wifi.el	
 else
     /usr/bin/emacs24-x -Q -l /home/pi/scripts/enable_wifi.el	
@@ -36,7 +37,7 @@ while :; do
 		Mon|Tue|Wed|Thu|Fri)
 		    /usr/bin/emacs24-x -Q -l /home/pi/scripts/enable_wifi.el ;;
 	    esac ;;
-	08:00)
+	06:45)
 	    case $currentdayoftheweek in
 		Sat|Sun)
 		    /usr/bin/emacs24-x -Q -l /home/pi/scripts/enable_wifi.el ;;
